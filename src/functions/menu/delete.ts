d512248186f7e2abeaab33e6bcd883ced16518f8
arthur <contato.arthurreis222@gmail.com>
2025-09-21 20:41:38 -0300

    feat: delete menu

---
+++ src/functions/menu/delete.ts
@@ -0,0 +1,15 @@
+import prisma from "../../database/prisma.js";
+import { NotFoundError } from "../../utils/errors-handlers.js";
+
+export const deleteMenu = async (menuId: string) => {
+  const existsMenu = await prisma.menu.findUnique({
+    where: {
+      id: menuId,
+    },
+  });
+  if (!existsMenu) throw new NotFoundError("O menu não foi encontrado");
+
+  await prisma.menu.delete({
+    where: { id: menuId },
+  });
+};
--- src/routes/menu.ts
+++ src/routes/menu.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { createMenu } from "../functions/menu/create.js";
 import { createMenuSchema } from "../zod/menu/create-menu-schema.js";
 import { getMenus } from "../functions/menu/get.js";
+import { deleteMenuSchema } from "../zod/menu/delete-menu-schema.js";
+import { deleteMenu } from "../functions/menu/delete.js";
 
 const menuRoutes = Router();
 
@@ -14,6 +16,12 @@ menuRoutes.post("/create", async (req, res) => {
 menuRoutes.get("/", async (req, res) => {
   const menus = await getMenus();
   return res.status(200).json({ menus });
+});
+
+menuRoutes.delete("/:menuId", async (req, res) => {
+  const data = deleteMenuSchema.parse(req.params);
+  await deleteMenu(data.menuId);
+  return res.sendStatus(204);
 });
 
 export default menuRoutes;
